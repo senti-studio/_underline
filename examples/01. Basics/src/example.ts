d7d667026bdaa95a88ff2a0803eed19f4c8ac6e2
Cristian Cornea <tinytales@mailbox.org>
2024-01-25 08:10:12 +0100

    example update

---
+++ examples/01. Basics/src/example.ts
@@ -0,0 +1,40 @@
+import { _u } from '@tinytales/_underline'
+
+export default function runExample() {
+  row1()
+  row2()
+}
+
+const row1 = () => {
+  _u.begin('row1')
+  // Moving the parent position moves all children
+  _u.position(100, 0)
+
+  _u.begin('row1 > red')
+  _u.fill('#FF0000')
+  _u.dimension(150, 150)
+
+  _u.begin('row1 > green')
+  _u.fill('#00FF44')
+  _u.dimension(150, 150)
+  // To not have green and blue stack on top of red
+  // We have to manually move the position
+  // (based of the parents position)
+  _u.position(150, 0)
+
+  _u.begin('row1 > blue')
+  _u.fill('#0033FF')
+  _u.dimension(150, 150)
+  // Here flex row would have been the better choice,
+  // But we are just starting out ;)
+  _u.position(300, 0)
+}
+
+const row2 = () => {
+  _u.begin('row2')
+  _u.position(0, 250)
+
+  _u.begin('row2 > red')
+  _u.fill('#FF0000')
+  _u.dimension(150, 150)
+}
--- examples/01. Basics/src/main.ts
+++ examples/01. Basics/src/main.ts
@@ -1,5 +1,31 @@
-import { _ } from "@tinytales/_underline"
+import { _u } from '@tinytales/_underline'
+import * as PIXI from 'pixi.js'
+import runExample from './example'
 
-_.begin()
-_.fill("#fff")
-_.end()
+const canvas = document.getElementById('app') as HTMLCanvasElement
+const app = new PIXI.Application({
+  background: '#000',
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  antialias: false,
+  width: window.innerWidth - 25,
+  height: window.innerHeight - 25,
+})
+canvas.appendChild(app.view)
+
+const scene = new PIXI.Container()
+app.stage.addChild(scene)
+
+runExample()
+
+_u.renderTo({
+  container: scene,
+  dimensions: {
+    w: window.innerWidth - 25,
+    h: window.innerHeight - 25,
+  },
+  position: {
+    x: 0,
+    y: 0,
+  },
+})
